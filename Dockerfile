5c1966a10784e6ee5e664c9464f8f93b160390c4
Grégoire Seux <kamaradclimber@gmail.com>
2018-02-24 14:24:24 +0100

    [CI] Build package in container

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM base/archlinux
+
+RUN echo $'\n[archlinuxfr]\n\
+SigLevel = Never\n\
+Server = http://repo.archlinux.fr/$arch' >> /etc/pacman.conf
+
+RUN pacman -Sy
+RUN pacman -S --noconfirm base-devel sudo yaourt python
+
+RUN useradd -m -s /bin/bash -d /tmp build 
+RUN echo "build ALL=NOPASSWD: ALL" >> /etc/sudoers
+
+CMD ["/bin/bash"]
--- build_package.sh
+++ build_package.sh
@@ -1,4 +1,38 @@
 #!/usr/bin/bash
 
+set -ex
+
 echo "Building archlinux package within docker"
-true
+
+# cleaning
+rm -rf home-assistant-*pkg.tar.*
+rm -rf src/*
+rm -rf 0.*.tar.gz
+
+if [[ -n "$UPDATE" ]]; then
+
+  pacman -S --noconfirm ruby
+
+  LAST_VERSION=$(ruby -r "net/http" -r "uri" -r "json" -e 'uri = URI.parse("https://api.github.com/repos/home-assistant/home-assistant/releases"); response = Net::HTTP.get_response(uri); if response.code.to_i != 200 then puts response.code.inspect; exit(1); end; puts JSON.parse(response.body).first["name"]')
+
+  VERSION=$1
+  echo "Will update to ${VERSION:=$LAST_VERSION}"
+  sed -i -re "s/^pkgver=.*$/pkgver=$VERSION/" PKGBUILD
+  # Reset release to 1
+  sed -i -re "s/^pkgrel=.*$/pkgrel=1/" PKGBUILD
+
+  sudo -u build makepkg --verifysource || echo "Will now update checksums"
+
+  sudo updpkgsums
+
+  sudo -u build makepkg --printsrcinfo > .SRCINFO
+fi
+
+# install 3 known aur dependencies
+sudo -u build yaourt -S --noconfirm --needed python-vincenty python-voluptuous python-aiohttp-cors python-astral
+
+
+sudo -u build makepkg -s -i --noconfirm
+echo "Package built!"
+
+sudo -u hass $(grep 'ExecStart' home-assistant.service | cut -d= -f2-)
